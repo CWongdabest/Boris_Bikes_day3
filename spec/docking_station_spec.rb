f7841c06e7d0bc6232916398f5701f7e03e3f67f
Canace Wong <canacewongtoday@hotmail.com>
2017-09-28 06:42:22 -0700

    gets bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
+  subject(:docking_station) {DockingStation.new}
   it 'should instantiate a new docking station' do
-    expect(subject).to be_instance_of(DockingStation)
+    expect(docking_station).to be_instance_of(DockingStation)
   end
 
   it { should respond_to(:release_bike) }
+
+  it 'release_bike gets a bike from the docking station' do
+    bike = Bike.new
+    expect(docking_station.release_bike).to eq(bike)
+  end
+
 end
