45b237ce998f50ad995e4a6679c7cdfd7b089c65
Canace Wong <canacewongtoday@hotmail.com>
2017-09-28 04:52:02 -0700

    updated test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,8 @@
 require 'docking_station'
 
-describe DockingStation.new do
-  it 'should instatiate a new docking station' do
-    expects(DockingStation.new).to be_valid
+describe DockingStation do
+  it 'should instantiate a new docking station' do
+    expect(subject).to be_instance_of(DockingStation)
   end
 
   it { should respond_to(:release_bike) }
